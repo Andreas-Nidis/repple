187c2c131941087de3fa3d4471ed55d5de30104e
Andreas Kalketenidis <andreas.nidis@MacBook-Air-5.local>
2025-06-19 12:49:32 +0200

    Created weightQueries.ts in backend db folder.

---
+++ backend/src/db/weightQueries.ts
@@ -0,0 +1,18 @@
+import { sql } from './db';
+
+export async function getWeightEntries(userId: string) {
+    return await sql `
+        SELECT week_start, weight FROM weekly_weight_logs 
+        WHERE user_id = $1 
+        ORDER BY week_start ASC
+    `, [userId];
+}
+
+export async function createOrUpdateWeightEntries(userId: string, weekStart: string, weight: number) {
+    await sql `
+        INSERT INTO weekly_weight_logs (user_id, week_start, weight)
+        VALUES ($1, $2, $3)
+        ON CONFLICT (user_id, week_start)
+        DO UPDATE SET weight = $3
+    `, [userId, weekStart, weight];
+};
