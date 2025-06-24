10b944422d5f3fde6d07a136cb7ccbed89cc12c6
Andreas Kalketenidis <andreas.nidis@MacBook-Air-5.local>
2025-06-24 16:14:09 +0200

    Created Workout Planning and Meal Planning buttons and paths on home screen.

--- frontend/app/(logged-in)/screens/index.tsx
+++ frontend/app/(logged-in)/screens/index.tsx
@@ -1,9 +1,10 @@
-import { SafeAreaView, StyleSheet} from 'react-native';
+import { SafeAreaView, StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 import { getAuth } from '@react-native-firebase/auth';
 import { useRouter } from 'expo-router';
 import HomeHeader from '@/components/screenComponents/home/HomeHeader';
 import HomeWeekCarousel from '@/components/screenComponents/home/HomeWeekCarousel';
 import WeightChart from '@/components/screenComponents/home/WeightChart';
+import MaterialDesignIcons from '@react-native-vector-icons/material-design-icons';
 
 export default function HomePage() {
   const router = useRouter();
@@ -17,6 +18,36 @@ export default function HomePage() {
       <HomeHeader />
       <HomeWeekCarousel />
       <WeightChart />
+      <View style={styles.planningContainer}>
+        <TouchableOpacity onPress={() => {router.push('/(logged-in)/screens/workouts')}}>
+          <View style={styles.planningButton}>
+            <Text style={styles.planningText}>
+              Workout {'\n'}
+              Planning
+            </Text>
+            <MaterialDesignIcons
+                style={{padding: 5}} 
+                name="weight-lifter"
+                size={30}
+                color='black'
+            />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => {router.push('/(logged-in)/screens/meals')}}>
+          <View style={styles.planningButton}>
+            <Text style={styles.planningText}>
+              Meal {'\n'}
+              Planning
+            </Text>
+            <MaterialDesignIcons
+                style={{padding: 5}} 
+                name="pot-steam-outline"
+                size={30}
+                color='black'
+            />
+          </View>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
@@ -27,21 +58,24 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
   },
-  modalContainer: {
-    flex: 1,
-    margin: 40,
-    backgroundColor: 'white',
-    borderRadius: 8,
-    padding: 20,
+  planningContainer: {
+    // marginTop: 5,
+    // borderWidth: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    height: 200,
+    width: '95%',
+  },
+  planningButton: {
+    borderWidth: 1,
+    borderRadius: 5,
     justifyContent: 'center',
     alignItems: 'center',
-    shadowColor: '#000',
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
+    height: 110,
+    width: 150,
   },
-  modalTitle: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    marginBottom: 12,
-  },
+  planningText: {
+    textAlign: 'center',
+  }
 });
