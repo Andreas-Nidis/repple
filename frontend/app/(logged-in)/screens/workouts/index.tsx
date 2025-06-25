358295bcab990c63f52c3f9a23979d0a250fa4b4
Andreas Kalketenidis <andreas.nidis@MacBook-Air-5.local>
2025-06-25 15:29:26 +0200

    Fleshed out /workouts landing page. Created FlatList and Add Button plus Modal.

--- frontend/app/(logged-in)/screens/workouts/index.tsx
+++ frontend/app/(logged-in)/screens/workouts/index.tsx
@@ -1,10 +1,92 @@
-import { SafeAreaView, View, Text, StyleSheet, TouchableOpacity } from 'react-native'
-import React from 'react'
+import { SafeAreaView, View, Text, StyleSheet, TouchableOpacity, FlatList, Modal, TextInput, Button } from 'react-native'
+import React, { useState, useEffect } from 'react'
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialDesignIcons from '@react-native-vector-icons/material-design-icons';
 import { useRouter } from 'expo-router';
+import { getAuth } from '@react-native-firebase/auth';
 
-const index = () => {
+type Workout = {
+  name: string;
+  category: string;
+  // add other properties if needed
+};
+
+const Index = () => {
   const router = useRouter();
+  const [workouts, setWorkouts] = useState<Workout[]>([]);
+  const [newWorkoutName, setNewWorkoutName] = useState('');
+  const [newWorkoutCategory, setNewWorkoutCategory] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const getWorkoutEntries = async () => {
+    try {
+      const user = getAuth().currentUser;
+      const idToken = await user?.getIdToken();
+      const response = await fetch('http://localhost:3001/api/workouts', {
+        headers: {
+          Authorization: `Bearer ${idToken}`,
+        }
+      })
+
+      if (!response.ok) {
+        const errorText = await response.text();
+        console.error('API returned error:', errorText);
+        return;
+      }
+
+      const data = await response.json();
+      setWorkouts(data);
+    } catch (error) {
+      console.log('Error fetching and setting weight data:', error);
+    }
+  }
+
+  const addWorkoutEntry = async () => {
+    try {
+      const user = getAuth().currentUser;
+      const idToken = await user?.getIdToken();
+      console.log(newWorkoutName, newWorkoutCategory);
+      const response = await fetch('http://localhost:3001/api/workouts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${idToken}`,
+        },
+        body: JSON.stringify({
+          name: newWorkoutName,
+          category: newWorkoutCategory,
+        })
+      })
+
+      if (!response.ok) {
+        const errorText = await response.text();
+        console.error('API returned error:', errorText);
+        return;
+      }
+
+      await getWorkoutEntries();
+      setModalVisible(false);
+      setNewWorkoutName('');
+      setNewWorkoutCategory('');
+    } catch (error) {
+      console.log('Error fetching and setting weight data:', error);
+    }
+  }
+
+  useEffect(() => {
+    getWorkoutEntries();
+  }, []);
+
+  const Item = ({name, category }: {name: string, category: string}) => (
+    <TouchableOpacity onPress={() => router.push('/(logged-in)/screens/workouts/[workoutId]')}>
+      <View style={styles.workoutBox}>
+        <Text style={styles.boxText}>{name}</Text>
+        <Text style={styles.boxText}>{category}</Text>
+      </View>
+    </TouchableOpacity>
+  )
+  
+
 
   return (
     <SafeAreaView style={styles.container}>
@@ -19,7 +101,50 @@ const index = () => {
       </View>
 
       <View style={{flex: 1}}>
-
+        <View>
+          {workouts.length < 1 ? ( <Text>Create a workout!</Text> ) : (
+            <FlatList 
+              data={workouts}
+              contentContainerStyle={{ alignItems: 'center' }}
+              renderItem={({ item }) =>  <Item name={item.name} category={item.category} />}
+            />
+          )}
+        </View>
+        <View>
+          <TouchableOpacity onPress={() => setModalVisible(true)}>
+            <MaterialDesignIcons name='plus-circle-outline' size={24} color='black' />
+            <Text>Add Workout</Text>
+          </TouchableOpacity>
+          <Modal 
+            visible={modalVisible}
+            animationType='slide'
+            transparent={true}
+            onRequestClose={() => setModalVisible(false)}
+          >
+            <View style={styles.modalBackground}>
+              <View style={styles.modalContent}>
+                <Text style={styles.modalTitle}>Add Workout Name:</Text>
+                <TextInput
+                  placeholder="Enter new workout name"
+                  keyboardType="default"
+                  value={newWorkoutName}
+                  onChangeText={setNewWorkoutName}
+                  style={styles.input}
+                />
+                <Text style={styles.modalTitle}>Add Workout Category:</Text>
+                <TextInput
+                  placeholder="Push, pull, legs, upper, etc."
+                  keyboardType="default"
+                  value={newWorkoutCategory}
+                  onChangeText={setNewWorkoutCategory}
+                  style={styles.input}
+                />
+                <Button title="Cancel" color="gray" onPress={() => setModalVisible(false)} />
+                <Button title="Save" onPress={() => addWorkoutEntry()} />
+              </View>
+            </View>
+          </Modal>
+        </View>
       </View>
 
       <View style={styles.footer}>
@@ -35,7 +160,7 @@ const index = () => {
   )
 }
 
-export default index
+export default Index
 
 const styles = StyleSheet.create({
   container: {
@@ -78,5 +203,44 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: '600',
     marginLeft: 5,
+  },
+  modalBackground: {
+      flex: 1,
+      backgroundColor: 'rgba(0,0,0,0.5)',
+      justifyContent: 'center',
+      alignItems: 'center',
+  },
+  modalContent: {
+      width: '85%',
+      backgroundColor: 'white',
+      padding: 20,
+      borderRadius: 16,
+      elevation: 10,
+  },
+  modalTitle: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      marginBottom: 16,
+      textAlign: 'center'
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    borderRadius: 8,
+    marginBottom: 12,
+  },
+  boxText: {
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  workoutBox: {
+    flexDirection: 'row',
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '80%',
   }
 })
